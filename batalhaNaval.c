683bf4c6d83e5a66f406c6161f80479fb779343c
MateusManganelli <mateusmanganelli@hotmail.com>
2025-03-31 14:48:25 +0000

    Nível Mestre

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -7,7 +7,7 @@ void posicionarNavios(int tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO]) {
     int x_vertical = 2, y_vertical = 3; // Coordenada inicial do navio vertical
     int x_horizontal = 5, y_horizontal = 6; // Coordenada inicial do navio horizontal
     int x_diagonal1 = 1, y_diagonal1 = 1; // Coordenada inicial do primeiro navio diagonal
-    int x_diagonal2 = 7, y_diagonal2 = 2; // Coordenada inicial do segundo navio diagonal
+    int x_diagonal2 = 7, y_diagonal2 = 7; // Coordenada inicial do segundo navio diagonal
     
     // Posiciona o navio verticalmente
     for (int i = 0; i < TAMANHO_NAVIO; i++) {
@@ -19,14 +19,45 @@ void posicionarNavios(int tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO]) {
         tabuleiro[x_horizontal][y_horizontal + i] = 2;
     }
     
-    // Posiciona o primeiro navio na diagonal (↖)
+    // Posiciona o navio diagonalmente (")
     for (int i = 0; i < TAMANHO_NAVIO; i++) {
         tabuleiro[x_diagonal1 + i][y_diagonal1 + i] = 3;
     }
     
-    // Posiciona o segundo navio na diagonal (↘)
+    // Posiciona o navio diagonalmente (/)
     for (int i = 0; i < TAMANHO_NAVIO; i++) {
         tabuleiro[x_diagonal2 - i][y_diagonal2 + i] = 3;
+    }
+}
+
+void aplicarHabilidade(int tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO], int x, int y, char tipo) {
+    if (tipo == 'C') { // Habilidade Cone
+        int cone[3][5] = {{0, 0, 1, 0, 0}, {0, 1, 1, 1, 0}, {1, 1, 1, 1, 1}};
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (cone[i][j] == 1 && x + i < TAMANHO_TABULEIRO && y - 2 + j >= 0 && y - 2 + j < TAMANHO_TABULEIRO) {
+                    tabuleiro[x + i][y - 2 + j] = 1;
+                }
+            }
+        }
+    } else if (tipo == 'O') { // Habilidade Octaedro
+        int octaedro[3][5] = {{0, 0, 1, 0, 0}, {0, 1, 1, 1, 0}, {0, 0, 1, 0, 0}};
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (octaedro[i][j] == 1 && x + i < TAMANHO_TABULEIRO && y - 2 + j >= 0 && y - 2 + j < TAMANHO_TABULEIRO) {
+                    tabuleiro[x + i][y - 2 + j] = 1;
+                }
+            }
+        }
+    } else if (tipo == 'X') { // Habilidade Cruz
+        int cruz[3][5] = {{0, 0, 1, 0, 0}, {1, 1, 1, 1, 1}, {0, 0, 1, 0, 0}};
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (cruz[i][j] == 1 && x + i < TAMANHO_TABULEIRO && y - 2 + j >= 0 && y - 2 + j < TAMANHO_TABULEIRO) {
+                    tabuleiro[x + i][y - 2 + j] = 1;
+                }
+            }
+        }
     }
 }
 
@@ -44,6 +75,12 @@ int main() {
     int tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO] = {0}; // Inicializa o tabuleiro com zeros
     
     posicionarNavios(tabuleiro);
+    
+    // Aplicação de habilidades
+    aplicarHabilidade(tabuleiro, 4, 4, 'C'); // Cone
+    aplicarHabilidade(tabuleiro, 7, 2, 'O'); // Octaedro
+    aplicarHabilidade(tabuleiro, 2, 7, 'X'); // Cruz
+    
     exibirTabuleiro(tabuleiro);
     
     return 0;
