5e40073fb170d454f22613b31bc21413c2d0e028
MateusManganelli <mateusmanganelli@hotmail.com>
2025-03-31 14:44:23 +0000

    Nível Aventureiro

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -6,6 +6,8 @@
 void posicionarNavios(int tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO]) {
     int x_vertical = 2, y_vertical = 3; // Coordenada inicial do navio vertical
     int x_horizontal = 5, y_horizontal = 6; // Coordenada inicial do navio horizontal
+    int x_diagonal1 = 1, y_diagonal1 = 1; // Coordenada inicial do primeiro navio diagonal
+    int x_diagonal2 = 7, y_diagonal2 = 2; // Coordenada inicial do segundo navio diagonal
     
     // Posiciona o navio verticalmente
     for (int i = 0; i < TAMANHO_NAVIO; i++) {
@@ -15,6 +17,16 @@ void posicionarNavios(int tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO]) {
     // Posiciona o navio horizontalmente
     for (int i = 0; i < TAMANHO_NAVIO; i++) {
         tabuleiro[x_horizontal][y_horizontal + i] = 2;
+    }
+    
+    // Posiciona o primeiro navio na diagonal (↖)
+    for (int i = 0; i < TAMANHO_NAVIO; i++) {
+        tabuleiro[x_diagonal1 + i][y_diagonal1 + i] = 3;
+    }
+    
+    // Posiciona o segundo navio na diagonal (↘)
+    for (int i = 0; i < TAMANHO_NAVIO; i++) {
+        tabuleiro[x_diagonal2 - i][y_diagonal2 + i] = 3;
     }
 }
 
